17c10b56bcd05b192b70b420744063fd23a687d7
Joseph  Cota <Joseph.Cota@kearsarge.org>
2023-04-13 08:39:44 -0400

    put in wall code

--- Wall.py
+++ Wall.py
@@ -1,1 +1,11 @@
-from Main import *
+import pygame, sys, math
+
+class Wall ():
+    def __init__(self, pos=[250, 250]):
+        self.image = pygame.image.load("images/tiles/Wall.png")
+        self.rect = self.image.get_rect(center = pos)
+        
+        self.kind = "wall"
+        
+    def update (self, size):
+        pass
