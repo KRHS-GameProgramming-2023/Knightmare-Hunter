1cf097b82799613ffab94e9c981d1ced2733cec5
Riley Dunn <riey.dunn@kearsarge.org>
2023-05-04 08:54:36 -0400

    this is copied from ball hud so still wip

--- Hud.py
+++ Hud.py
@@ -1,1 +1,7 @@
+import pygame, sys, math
 
+class Hud():
+    def __init__(self, pos=[50,50]):
+        self.font = pg.font.Font(None, 64)
+        self.image = self.font.render("Score: 0", True, (0, 0, 0))
+        self.rect = slef.image.get_rect(center = startPos)
