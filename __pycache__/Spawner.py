11b1d0e55af2f49c4a626ec8e98fa6746a85c9a1
Joseph  Cota <Joseph.Cota@kearsarge.org>
2023-05-05 08:29:22 -0400

    Fixed name

--- __pycache__/Spawner.py
+++ __pycache__/Spawner.py
@@ -1,11 +1,3 @@
 import pygame, sys, math
 
 class Spawner ():
-    def _init_(self, pos=[50, 25]):
-        self.image = pygame.image.load("images/tiles/Spawner.png")
-        self.rect = self.image.get_rect(center = pos)
-        
-        self.kind = "Spawner"
-        
-    def update(self, size):
-        pass
